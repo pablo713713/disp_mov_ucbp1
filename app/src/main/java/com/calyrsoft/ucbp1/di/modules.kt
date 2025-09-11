3a72dbf00b187e5d05e03e25446d0e3c34bbc9a5
pablo <latorrepablo775@gmail.com>
2025-09-11 00:54:13 -0400

    clean code feature: github

--- app/src/main/java/com/calyrsoft/ucbp1/di/modules.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/di/modules.kt
@@ -1,9 +1,8 @@
 package com.calyrsoft.ucbp1.di
 
-import com.calyrsoft.ucbp1.features.github.data.remote.GithubApi
+import com.calyrsoft.ucbp1.features.github.data.api.GithubService
+import com.calyrsoft.ucbp1.features.github.data.datasource.GithubRemoteDataSource
 
-
-import com.calyrsoft.ucbp1.features.github.data.repository.GithubRepository
 import com.calyrsoft.ucbp1.features.github.domain.repository.IGithubRepository
 import com.calyrsoft.ucbp1.features.github.domain.usecase.FindByNickNameUseCase
 import com.calyrsoft.ucbp1.features.github.presentation.GithubViewModel
@@ -18,27 +17,43 @@ import com.calyrsoft.ucbp1.features.dollar.data.repository.DollarQuote
 import com.calyrsoft.ucbp1.features.dollar.domain.repository.IDollarQuote
 import com.calyrsoft.ucbp1.features.dollar.domain.usecase.FindDollarQuoteUseCase
 import com.calyrsoft.ucbp1.features.dollar.presentation.DollarViewModel
+import com.calyrsoft.ucbp1.features.github.data.repository.GithubRepository
+import okhttp3.OkHttpClient
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val appModule = module {
+
+    single {
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
 
     single {
         Retrofit.Builder()
             .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(GithubApi::class.java)
     }
 
 
     // 📌 Github
+    single<GithubService> {
+        get<Retrofit>().create(GithubService::class.java)
+    }
+    single{ GithubRemoteDataSource(get()) }
     single<IGithubRepository> { GithubRepository(get()) }
+
     factory { FindByNickNameUseCase(get()) }
-    viewModel { GithubViewModel(get()) }
+    viewModel { GithubViewModel(get(), get()) }
+
 
     // 📌 Login
     single<ILoginRepository> { LoginRepository() }
--- app/src/main/java/com/calyrsoft/ucbp1/features/github/data/repository/GithubRepository.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/features/github/data/repository/GithubRepository.kt
@@ -1,37 +1,38 @@
 package com.calyrsoft.ucbp1.features.github.data.repository
 
-import com.calyrsoft.ucbp1.features.github.data.remote.GithubApi
+import com.calyrsoft.ucbp1.features.github.data.datasource.GithubRemoteDataSource
+import com.calyrsoft.ucbp1.features.github.data.error.DataException
+import com.calyrsoft.ucbp1.features.github.domain.error.Failure
 import com.calyrsoft.ucbp1.features.github.domain.model.UserModel
 import com.calyrsoft.ucbp1.features.github.domain.repository.IGithubRepository
 
 class GithubRepository(
-    private val api: GithubApi
-) : IGithubRepository {
-
+    val remoteDataSource: GithubRemoteDataSource
+): IGithubRepository {
     override suspend fun findByNick(value: String): Result<UserModel> {
-        if (value.isEmpty()) {
-            return Result.failure(Exception("El campo no puede estar vacío"))
+        if(value.isEmpty()) {
+            return Result.failure(Exception("El campo no puede estar vacio"))
         }
+        val response = remoteDataSource.getUser(value)
 
-        return try {
-            val response = api.getUser(value)
-            if (response.isSuccessful) {
-                val body = response.body()
-                if (body != null) {
-                    Result.success(
-                        UserModel(
-                            nickname = body.login,
-                            pathUrl = body.avatar_url
-                        )
-                    )
-                } else {
-                    Result.failure(Exception("Usuario no encontrado"))
+        response.fold(
+            onSuccess = {
+                    it ->
+                return Result.success(UserModel(
+                    nickname = it.login,
+                    pathUrl = it.url
+                ))
+            },
+            onFailure = { exception ->
+                val failure = when (exception) {
+                    is DataException.Network -> Failure.NetworkConnection
+                    is DataException.HttpNotFound -> Failure.NotFound
+                    is DataException.NoContent -> Failure.EmptyBody
+                    is DataException.Unknown -> Failure.Unknown(exception)
+                    else -> Failure.Unknown(exception)
                 }
-            } else {
-                Result.failure(Exception("Error ${response.code()}"))
+                return Result.failure(failure)
             }
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
+        )
     }
 }
